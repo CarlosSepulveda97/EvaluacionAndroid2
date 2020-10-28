14d4de3582b81e76a52170c11363472fc56bc097
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-10-28 03:49:29 -0300

    Falta prestamos, bbdd y el login (5/7)

--- app/src/main/java/com/example/evaluacion2android/Maps_Act.java
+++ app/src/main/java/com/example/evaluacion2android/Maps_Act.java
@@ -1,16 +1,46 @@
 package com.example.evaluacion2android;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class Maps_Act extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Maps_Act extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps_);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_maps_2);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // 53.577110, 8.588951 casa bremerhaven
+        LatLng sydney = new LatLng(53.577110, 8.588951);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Casa Charlotte"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/com/example/evaluacion2android/Seguridad_Act.java
+++ app/src/main/java/com/example/evaluacion2android/Seguridad_Act.java
@@ -3,12 +3,74 @@ package com.example.evaluacion2android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Seguridad_Act extends AppCompatActivity {
+
+
+    private EditText edit;
+    private TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seguridad_);
+        getSupportActionBar().hide();
+
+        edit = (EditText) findViewById(R.id.edit);
+        tv = (TextView) findViewById(R.id.textView);
+
+
+
     }
+
+    private SecretKeySpec generateKey(String password) throws  Exception{
+
+        MessageDigest sha = MessageDigest.getInstance("SHA-256");
+        byte[] key = password.getBytes("UTF-8");
+        key = sha.digest(key);
+        SecretKeySpec secretKey = new SecretKeySpec(key,"AES");
+
+        return secretKey;
+    }
+
+    private String encriptar (String datos, String password) throws Exception{
+
+        if (!edit.getText().toString().isEmpty()){
+
+            SecretKeySpec secretKey = generateKey(password);
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            byte[] datosEncriptadosBytes = cipher.doFinal(datos.getBytes());
+            String datosEncriptadosString = Base64.encodeToString(datosEncriptadosBytes, Base64.DEFAULT);
+            return datosEncriptadosString;
+
+        }else{
+            Toast.makeText(this, "Debe ingresar una clave", Toast.LENGTH_LONG).show();
+            return null;
+        }
+
+
+    }
+
+    public void btn(View view){
+        try {
+            String mensaje = encriptar(edit.getText().toString(),tv.getText().toString());
+            tv.setText(mensaje);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
