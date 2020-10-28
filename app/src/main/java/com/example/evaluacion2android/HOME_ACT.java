f3c77327264640efa31a8d7cd5f0d331fcd82e95
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-10-27 22:20:06 -0300

    Diseño completo de la aplicacion

--- app/src/main/java/com/example/evaluacion2android/HOME_ACT.java
+++ app/src/main/java/com/example/evaluacion2android/HOME_ACT.java
@@ -4,16 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HOME_ACT extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-
-
-
-
+        setContentView(R.layout.activity_h_o_m_e__a_c_t);
+        getSupportActionBar();
     }
 }
--- app/src/main/java/com/example/evaluacion2android/Info_Act.java
+++ app/src/main/java/com/example/evaluacion2android/Info_Act.java
@@ -4,16 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Info_Act extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_info_);
         getSupportActionBar().hide();
-
-
-
-
     }
 }
